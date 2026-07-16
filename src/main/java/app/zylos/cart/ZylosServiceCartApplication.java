89a5dac1128c72d92f9d277919a731c8e5ae2b6d
Kamesh Chathuranga <kameshchathuranga7@gmail.com>
2026-07-16 15:43:32 +0530

    initialize zylos-service-cart project with Maven and Spring Boot setup

---
+++ src/main/java/app/zylos/cart/ZylosServiceCartApplication.java
@@ -0,0 +1,13 @@
+package app.zylos.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZylosServiceCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZylosServiceCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/zylos/cart/ZylosServiceCartApplicationTests.java
@@ -0,0 +1,13 @@
+package app.zylos.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZylosServiceCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
